f8532a5836264b5e31e3b30cb3fa584b4a834219
xhoang0509 <xhoang0509@gmail.com>
2021-12-16 09:54:12 +0700

    dynamic data in template

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+    const name = "Xuan Hoang";
+    const year = 2021;
+    const course = {
+        name: "ReactJS",
+        price: "100$",
+    };
+    
     return (
         <div className="App">
+            {console.log(course)}
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h2>Hello React App, I'm Xuan Hoang</h2>
+                <h2>
+                    Hello React App, I'm {name} in {year}
+                </h2>
+                <h3>Course: {JSON.stringify(course)}</h3>
+                
             </header>
         </div>
     );
