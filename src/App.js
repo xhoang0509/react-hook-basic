4b0a6b3d7cfd51ec0808802cd6ca40fe623f5208
xhoang0509 <xhoang0509@gmail.com>
2021-12-16 10:20:37 +0700

    add styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import Nav from "./components/Nav";
+
 const App = () => {
     const name = "Xuan Hoang";
     const year = 2021;
@@ -8,17 +10,18 @@ const App = () => {
         name: "ReactJS",
         price: "100$",
     };
-    
+
     return (
         <div className="App">
-            {console.log(course)}
+            <Nav />
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <h2>
                     Hello React App, I'm {name} in {year}
                 </h2>
-                <h3>Course: {JSON.stringify(course)}</h3>
-                
+                <h3 style={{ color: "red", fontSize: "16px" }}>
+                    Course: {JSON.stringify(course)}
+                </h3>
             </header>
         </div>
     );
